ca05cc28cc6aa77b9491845a1510f283d3f2c437
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-17 19:40:32 +0430

    Add files via upload

---
+++ linkshort.py
@@ -0,0 +1,34 @@
+# Check the requirements
+
+try:
+    import pyshorteners
+    from colorama import Fore
+    import os
+    from os import system
+except ImportError:
+   system("pip install os ")
+   system("pip install colorama ")
+   system("pip install pyshortenerse")
+   exit("\n\nRun script Again")
+
+# Start APP
+
+os.system("Cls")
+print(Fore.RED + """ /$$       /$$           /$$              /$$$$$$  /$$                             /$$                                            
+| $$      |__/          | $$             /$$__  $$| $$                            | $$                                            
+| $$       /$$ /$$$$$$$ | $$   /$$      | $$  \__/| $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$    /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$ 
+| $$      | $$| $$__  $$| $$  /$$/      |  $$$$$$ | $$__  $$ /$$__  $$ /$$__  $$|_  $$_/   /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$
+| $$      | $$| $$  \ $$| $$$$$$/        \____  $$| $$  \ $$| $$  \ $$| $$  \__/  | $$    | $$$$$$$$| $$  \ $$| $$$$$$$$| $$  \__/
+| $$      | $$| $$  | $$| $$_  $$        /$$  \ $$| $$  | $$| $$  | $$| $$        | $$ /$$| $$_____/| $$  | $$| $$_____/| $$      
+| $$$$$$$$| $$| $$  | $$| $$ \  $$      |  $$$$$$/| $$  | $$|  $$$$$$/| $$        |  $$$$/|  $$$$$$$| $$  | $$|  $$$$$$$| $$      
+|________/|__/|__/  |__/|__/  \__/       \______/ |__/  |__/ \______/ |__/         \___/   \_______/|__/  |__/ \_______/|__/      
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  """)
+def short(url):
+    link = pyshorteners.Shortener()
+    return link.chilpit.short(url)
+    
+
+url = input(Fore.GREEN+ "send Your URL  : ")
+print(Fore.WHITE+ f"Your short Url : {short(url)}")
